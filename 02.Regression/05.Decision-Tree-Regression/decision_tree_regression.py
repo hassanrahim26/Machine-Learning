0d97bfe9bb8750d8a2e73ed03385cfa07eb48cd6
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-01-29 20:23:17 +0530

    Create decision_tree_regression.py

---
+++ 02.Regression/05.Decision-Tree-Regression/decision_tree_regression.py
@@ -0,0 +1,37 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+# X contains the Position, Level columns
+y = dataset.iloc[:, -1].values
+# y contains the salary column
+
+# Training the Decision Tree Regression model on the whole dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+## DecisionTreeRegressor(random_state=0)
+
+# Predicting a new result
+regressor.predict([[6.5]])
+## array([150000.])
+
+## Decision Tree Regressor model is clearly not the best model to use on a single feature dataset. It's more adapted to dataset with many features, a high dimensional dataset.
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+## The decision tree regressor model is really not the best adapted to two dimensional dataset, with only one feature and one dependent variable.
